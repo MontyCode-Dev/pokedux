9a8f3e5b924292cebe229cf7c5e165bbe55cd87c
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2022-02-10 23:00:03 -0800

    Update Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Layout } from './Layout';
 export { default as Menu } from './Menu';
 export { default as SearchBar } from './SearchBar';
+export { default as Logo } from './Logo';
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,9 +5,6 @@ import { PokemonList } from '../components/pokemon';
 const Home = () => {
     return (
         <div className='Home'>
-          <h1 className="text-3xl font-bold underline">
-            Hello world!
-          </h1>
           <SearchBar />
           <PokemonList />
         </div>
